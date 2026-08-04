515f4ddb8a57fde717a39a31c098d306484f9c8a
03AJ03 <203957546+03AJ03@users.noreply.github.com>
2026-08-04 13:38:19 +0000

    LeetCode submission: 2094043796 - Product Sales Analysis III

--- 1155-product-sales-analysis-iii/solution.sql
+++ 1155-product-sales-analysis-iii/solution.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
-select product_id, year as first_year, quantity, price 
-from sales where (product_id, year) in (
-    select product_id, min(year)
-    from sales
-    group by product_id
+WITH min_year AS(
+    SELECT product_id,quantity,price,year,
+    DENSE_RANK() OVER (PARTITION BY product_id ORDER BY year asc) AS rnk
+    FROM Sales
 )
+SELECT product_id,year as first_year,quantity,price
+from min_year
+where rnk=1
