423622898313f54351fee3837c1378bd2cbbca19
03AJ03 <203957546+03AJ03@users.noreply.github.com>
2026-08-05 15:05:25 +0000

    LeetCode submission: 2095481344 - Movie Rating

--- 1480-movie-rating/solution.sql
+++ 1480-movie-rating/solution.sql
@@ -1,20 +1,24 @@
 # Write your MySQL query statement below
-(SELECT U.name as results
-FROM Users U
-JOIN MovieRating MR ON MR.user_id=U.user_id
-GROUP BY U.user_id , U.name
-ORDER BY COUNT(*) DESC , U.name asc
-LIMIT 1
-)
-UNION ALL
 (
-SELECT M.title as results
-FROM Movies M
-JOIN MovieRating MR ON MR.movie_id=M.movie_id
-WHERE MONTH(MR.created_at)='2' AND YEAR(MR.created_at)='2020'
-GROUP BY M.movie_id,M.title
-ORDER BY AVG(MR.rating) DESC,M.title asc
+SELECT u.name AS results
+FROM Users u
+JOIN MovieRating mr
+ON u.user_id = mr.user_id
+GROUP BY u.user_id, u.name
+ORDER BY COUNT(*) DESC, u.name ASC
 LIMIT 1
 )
 
+UNION ALL
 
+(
+SELECT m.title
+FROM Movies m
+JOIN MovieRating mr
+ON m.movie_id = mr.movie_id
+WHERE YEAR(created_at)=2020
+AND MONTH(created_at)=2
+GROUP BY m.movie_id, m.title
+ORDER BY AVG(rating) DESC, m.title ASC
+LIMIT 1
+);
